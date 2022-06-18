c3b825c9c81f1c4ba90cc210f6e065a85893f99e
sergey <urserg@gmail.com>
2022-06-18 15:12:49 +0300

    refactor(apollo-server): apollo-server.ts

--- src/apollo-server.ts
+++ src/apollo-server.ts
@@ -4,7 +4,7 @@ import { addResolversToSchema } from '@graphql-tools/schema';
 import {GRAPHQL_SCHEMA_PATH} from "./constants";
 import createResolvers from './resolvers';
 import * as express from 'express';
-import { ApolloServer, ExpressContext, Config } from 'apollo-server-express';
+import { ApolloServer, ExpressContext } from 'apollo-server-express';
 import { Server } from 'http';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 
@@ -12,41 +12,26 @@ const schema = loadSchemaSync(GRAPHQL_SCHEMA_PATH, {
   loaders: [new GraphQLFileLoader()],
 });
 
-async function startApolloServer(
-  serverOptions: { httpServer: Server; app: express.Application },
-  apolloOptions: {
-    schemaWithResolvers: Exclude<Config<ExpressContext>['schema'], undefined>;
-  },
-  models: Record<string, any>
-): Promise<ApolloServer<ExpressContext>> {
-  const {httpServer, app} = serverOptions;
-  const {schemaWithResolvers} = apolloOptions;
-  const server = new ApolloServer({
-    schema: schemaWithResolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
-    introspection: true,
-    context: () => ({models})
-  });
-  await server.start();
-  server.applyMiddleware({app});
-  return server;
-}
-
 export async function createApolloServer(
   models: Record<string, any>,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-  // Add resolvers to the schema
-  const schemaWithResolvers = addResolversToSchema({
-    schema,
-    resolvers: createResolvers(),
+  const server = new ApolloServer({
+    context: () =>
+      ({
+        models,
+        dbTweetCache: {},
+        dbTweetToFavoriteCountMap: {},
+        dbUserCache: {},
+      }),
+    schema: addResolversToSchema({
+      schema: schema,
+      resolvers: createResolvers(),
+    }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-
-  const apolloServer = await startApolloServer(
-    { httpServer, app },
-    { schemaWithResolvers },
-    models
-  );
-  return apolloServer;
+  await server.start();
+  server.applyMiddleware({ app });
+  return server;
 }
